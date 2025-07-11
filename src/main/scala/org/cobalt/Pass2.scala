5dd84113aa16c07baaa1ad977bb7aa8b929c124e
Gorilla <codegorilla@users.noreply.github.com>
2025-07-11 07:20:03 -0700

    Experiment with stringtemplate groups and dictionaries

--- src/main/scala/org/cobalt/Pass2.scala
+++ src/main/scala/org/cobalt/Pass2.scala
@@ -16,6 +16,9 @@ class Pass2 {
   var templateDir: URL = null
   var groupDir: STGroupDir = null
 
+  var templateGroupFile: URL = null
+  var groupFile: STGroupFile = null
+
   var counter: Int = 0
 
   var symbolTable: SymbolTable = null
@@ -31,6 +34,9 @@ class Pass2 {
   def process () =
     templateDir = this.getClass().getClassLoader().getResource("templates")
     groupDir = STGroupDir(templateDir);
+
+    templateGroupFile = this.getClass().getClassLoader().getResource("template-groups/modifiers.stg")
+    groupFile = STGroupFile(templateGroupFile)
     translationUnit(input)
 
   def translationUnit (current: AstNode) =
@@ -40,10 +46,31 @@ class Pass2 {
   
   def variableDeclaration (current: AstNode) =
     // Child 0 is modifiers
-    val st = name(current.getChild(1))
-    println(st.render())
+    val stm = modifiers(current.getChild(0))
+    println(stm.render())
+    // val stn = name(current.getChild(1))
+    // println(stn.render())
     // typeRoot(current.getChild(1))
 
+  // Modifiers are tricky for two reasons: First, keywords in the target
+  // language are not always the same as in the source language; and sometimes
+  // the mapping depends on context. For example, the keyword 'final' becomes
+  // 'const' for variables, but remains 'final' for classes. Second, the
+  // placement of modifiers might differ between source and target language. For
+  // example, in class declarations, 'final' shifts from before a class name to
+  // after. Also, the visibility modifiers (e.g. 'public', 'private') are lifted
+  // out of each individual method and the methods are then grouped under
+  // visibility "headings".
+
+  def modifiers (current: AstNode): ST =
+    val list = LinkedList[String]()
+    list.add("public")
+    list.add("static")
+    list.add("final")
+    val st = groupFile.getInstanceOf("modifiers")
+    st.add("mods", list)
+    return st
+                                                                 
   def name (current: AstNode): ST =
     val st = groupDir.getInstanceOf("name")
     st.add("name", current.getToken().lexeme)
